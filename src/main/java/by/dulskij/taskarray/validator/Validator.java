1715225e860b9d25000827a78a0d66983cc8df65
PavelDulskij <paveldulskij06@gmail.com>
2025-10-28 15:37:00 +0300

    Delete Validator.java

--- src/main/java/by/dulskij/taskarray/validator/Validator.java
+++
@@ -1,7 +0,0 @@
-package by.dulskij.taskarray.validator;
-
-import by.dulskij.taskarray.exception.ArrayFormatException;
-
-public interface Validator {
-    boolean isValidLine(String line) throws ArrayFormatException;
-}
