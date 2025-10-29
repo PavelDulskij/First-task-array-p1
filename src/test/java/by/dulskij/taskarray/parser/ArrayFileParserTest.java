3993db2921e144ad27e1cf627d7cb99ffe387f69
PavelDulskij <paveldulskij06@gmail.com>
2025-10-29 12:45:46 +0300

    Create ArrayFileParserTest.java

---
+++ src/test/java/by/dulskij/taskarray/parser/ArrayFileParserTest.java
@@ -0,0 +1,29 @@
+package by.dulskij.taskarray.parser;
+
+import by.dulskij.taskarray.entity.CustomArray;
+import by.dulskij.taskarray.exception.ArrayFormatException;
+import by.dulskij.taskarray.parser.impl.ArrayFileParserImpl;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayFileParserTest {
+    @Test
+    void whenParseLineThenReturnNewArray() throws ArrayFormatException {
+        ArrayFileParser parser = new ArrayFileParserImpl();
+
+        CustomArray array = parser.parseLine("1 2 3 4 5 6 7 4");
+        int[] actual = array.getArray();
+        int[] expected = new int[]{1, 2, 3, 4, 5, 6, 7, 4};
+
+        assertArrayEquals(expected,actual);
+    }
+
+    @Test
+    void whenParseInvalidLineThenReturnEmptyArray() throws ArrayFormatException {
+        ArrayFileParser parser = new ArrayFileParserImpl();
+        CustomArray array = parser.parseLine("1e 2x 3y 4few ,,jh5");
+
+        assertEquals(0, array.getArray().length);
+    }
+}
