f7b7a394d61edf01bcc515a546a3eacdac5fce72
PavelDulskij <paveldulskij06@gmail.com>
2025-10-29 14:40:41 +0300

    Update CustomArrayServices.java

--- src/main/java/by/dulskij/taskarray/service/CustomArrayServices.java
+++ src/main/java/by/dulskij/taskarray/service/CustomArrayServices.java
@@ -7,8 +7,8 @@ public interface CustomArrayServices {
     int findMin(CustomArray array) throws ArrayFormatException;
     int findMax(CustomArray array) throws ArrayFormatException;
     double calculateAverage(CustomArray array) throws ArrayFormatException;
-    int calculateSum(CustomArray array);
-    int calculatePositive(CustomArray array);
-    int calculateNegative(CustomArray array);
+    int calculateSum(CustomArray array) throws ArrayFormatException;;
+    long calculatePositiveCount(CustomArray array) throws ArrayFormatException;;
+    long calculateNegativeCount(CustomArray array) throws ArrayFormatException;;
     int[] replaceByCondition(int index, CustomArray array, int newValue) throws ArrayFormatException;
 }
