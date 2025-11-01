5f37103b4ed1ed69e5dd5960cf47633565e4887a
PavelDulskij <paveldulskij06@gmail.com>
2025-11-01 23:19:35 +0300

    Create StreamCustomArrayServiceImpl.java

---
+++ src/main/java/by/dulskij/taskarray/service/impl/StreamCustomArrayServiceImpl.java
@@ -0,0 +1,82 @@
+package by.dulskij.taskarray.service.impl;
+
+import by.dulskij.taskarray.entity.CustomArray;
+import by.dulskij.taskarray.exception.ArrayFormatException;
+import by.dulskij.taskarray.service.CustomArrayService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Arrays;
+
+public class StreamCustomArrayServiceImpl implements CustomArrayService {
+
+    private static final Logger logger = LogManager.getLogger(StreamCustomArrayServiceImpl.class);
+
+    @Override
+    public int findMin(CustomArray array) throws ArrayFormatException {
+        int[] arr = array.getArray();
+        return Arrays.stream(arr)
+                .min()
+                .orElseThrow(() -> new ArrayFormatException("Array is empty"));
+    }
+
+    @Override
+    public int findMax(CustomArray array) throws ArrayFormatException {
+        int[] arr = array.getArray();
+        return Arrays.stream(arr)
+                .max()
+                .orElseThrow(() -> new ArrayFormatException("Array is empty"));
+    }
+
+    @Override
+    public double calculateAverage(CustomArray array) throws ArrayFormatException {
+        int[] arr = array.getArray();
+        return Arrays.stream(arr)
+                .average()
+                .orElseThrow(() -> new ArrayFormatException("Array is empty"));
+    }
+
+    @Override
+    public int calculateSum(CustomArray array) throws ArrayFormatException{
+        checkArrayLength(array);
+
+        int[] arr = array.getArray();
+        return Arrays.stream(arr)
+                .sum();
+    }
+
+    @Override
+    public long calculatePositiveCount(CustomArray array) throws ArrayFormatException{
+        checkArrayLength(array);
+
+        int[] arr = array.getArray();
+        return Arrays.stream(arr)
+                .filter((n) -> n > 0)
+                .count();
+    }
+
+    @Override
+    public long calculateNegativeCount(CustomArray array) throws ArrayFormatException{
+        checkArrayLength(array);
+
+        int[] arr = array.getArray();
+        return Arrays.stream(arr)
+                .filter((n) -> n < 0)
+                .count();
+    }
+
+    @Override
+    public int[] replaceByCondition(CustomArray array) throws ArrayFormatException {
+        checkArrayLength(array);
+
+        return Arrays.stream(array.getArray())
+                .map((num) -> num < 0 ? num * 2 : num)
+                .toArray();
+    }
+
+    private void checkArrayLength(CustomArray array) throws ArrayFormatException{
+        if (array.getArray().length == 0) {
+            throw new ArrayFormatException("Array is empty");
+        }
+    }
+}
