df35c796455d60d42458bcbf628fc27852aecaee
PavelDulskij <paveldulskij06@gmail.com>
2025-11-04 12:19:38 +0300

    Create ArrayFileParserTest.java

---
+++ test/by/dulskij/taskarray/parser/ArrayFileParserTest.java
@@ -0,0 +1,43 @@
+package by.dulskij.taskarray.parser;
+
+import by.dulskij.taskarray.entity.CustomArray;
+import by.dulskij.taskarray.exception.ArrayFormatException;
+import by.dulskij.taskarray.parser.impl.ArrayFileParserImpl;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayFileParserTest {
+    @Test
+    void whenParseLineThenReturnNewArray() throws ArrayFormatException {
+        ArrayFileParser parser = new ArrayFileParserImpl();
+
+        CustomArray array = parser.parseLine("    1 2   3    4 5   6 7 4");
+        int[] actual = array.getArray();
+        int[] expected = new int[]{1, 2, 3, 4, 5, 6, 7, 4};
+
+        assertArrayEquals(expected,actual);
+    }
+
+    @Test
+    void whenParseInvalidLineThenReturnEmptyArray() throws ArrayFormatException {
+        ArrayFileParser parser = new ArrayFileParserImpl();
+        CustomArray array = parser.parseLine("1t 2e 3y 4few ,,jh5");
+
+        int[] expected = new int[0];
+        int[] actual = array.getArray();
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void whenParseEmptyLineThenReturnEmptyArray() throws ArrayFormatException {
+        ArrayFileParser parser = new ArrayFileParserImpl();
+        CustomArray array = parser.parseLine("");
+
+        int[] expected = new int[]{};
+        int[] actual = array.getArray();
+
+        assertArrayEquals(expected, actual);
+    }
+}
