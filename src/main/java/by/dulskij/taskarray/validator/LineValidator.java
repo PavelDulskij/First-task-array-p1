312ee0699b9d594e6564180148bc564e51068c2e
PavelDulskij <paveldulskij06@gmail.com>
2025-11-04 12:19:57 +0300

    Update LineValidator.java

--- src/main/java/by/dulskij/taskarray/validator/LineValidator.java
+++ src/main/java/by/dulskij/taskarray/validator/LineValidator.java
@@ -1,7 +1,5 @@
 package by.dulskij.taskarray.validator;
 
-import by.dulskij.taskarray.exception.ArrayFormatException;
-
 public interface LineValidator {
-    boolean isValidValueOfLine(String valueOfLine) throws ArrayFormatException;
+    boolean isValidValueOfLine(String valueOfLine);
 }
